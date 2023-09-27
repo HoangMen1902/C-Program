fb99dcce8f2c4a5626f37066b5586e11e4b27420
7CONonoka <hoangmen9918@gmail.com>
2023-09-27 11:34:12 +0700

    Fixed Power Price

--- Main.cpp
+++ Main.cpp
@@ -6,21 +6,22 @@ int TimUoc(int x, int y){
 }
 //---------------------------------------TINH TIEN DIEN----------------------------------
 int TienDien(int kwh){
-	int tien;
-    if(kwh <= 50 ){
-        tien = kwh * 1678;
-    }else if (  kwh > 50 && kwh <= 100 ){
-        tien = 50 * 1678 + (kwh - 50) * 1734;
-    } else if ( kwh > 100 && kwh <=200){
-        tien = 50 * 1678 + 50 * 1734 + (kwh - 100) * 2014;
-    } else if ( kwh > 200 && kwh <= 300){
-        tien = 50 * 1678 + 50 * 1734 + 100 * 2014 + (kwh - 100) * 2536;
-    } else if ( kwh > 300 && kwh <=400 ){
-        tien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (kwh - 100) *2834;
-    }else {
-        tien = 50 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + 100 * 2834 + ( kwh - 100) * 2927;
+	const int Bac1=50,Bac2=100,Bac3=200,Bac4=300,Bac5=400;
+	int tiendien;
+    if(kwh<50){
+        tiendien = Bac1*1678;
+    } else if (kwh<100 && kwh>=50){
+        tiendien = Bac1 * 1678 + (kwh - Bac1)*1734;
+    } else if(kwh<200 && kwh>=100){
+        tiendien = Bac1 * 1678 + 50 * 1734 + (kwh - Bac2)*2014;
+    } else if(kwh>200 && kwh<=300){
+        tiendien = Bac1 * 1678 + 50 * 1734 + 100 * 2014 + (kwh - Bac3)*2536; 
+    }else if(kwh>300 && kwh<=400){
+        tiendien = Bac1 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + (kwh - Bac4)*2834; 
+    }else if(kwh>400){
+        tiendien = Bac1 * 1678 + 50 * 1734 + 100 * 2014 + 100 * 2536 + 100 * 2834 + (kwh - Bac5) *2927;
     }
-    return tien;
+	return tiendien;
 }
 //--------------------------------TINH TIEN KARAOKE----------------------------------------------------------------
 int kara(int giobatdau, int gioketthuc){
